5e3bb8b8e585cd0cd00dcc1985047f3eb9884d7a
AbdulGit19 <saddam_9119@yahoo.in>
2019-12-02 19:50:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.scss";
+
+import TourList from "./components/TourList";
+
+export default class App extends Component {
+  render() {
+    return (
+      <main>
+        <Navbar />
+        <TourList />
+      </main>
+    );
+  }
+}
